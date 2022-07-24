d8fe1a33b43681e9662c8f03e24679ef1a1ab7ef
Jorge Carrera <JCarrera994@gmail.com>
2022-07-24 12:23:36 -0400

    adds validations; models; and controllers

---
+++ models/transactions.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    item_name: 'income',
+    amount: 1000,
+    date: 'January 22',
+    from: 'employer',
+    category: 'income'
+  },
+  {
+    item_name: 'income',
+    amount: 1000,
+    date: 'January 29',
+    from: 'employer',
+    category: 'income'
+  },
+  {
+    item_name: 'income',
+    amount: 1000,
+    date: 'Febuary 5',
+    from: 'employer',
+    category: 'income'
+  },
+];
---
+++ models/validations.js
@@ -0,0 +1,22 @@
+const validateValues = (req, res, next) => {
+  const {
+    item_name,
+    amount,
+    date,
+    from,
+    category,
+  } = req.body;
+  if (
+    typeof item_name !== "string" ||
+    typeof Number(amount) !== "number" ||
+    typeof date !== "string" ||
+    typeof from !== "string" ||
+    typeof category !== "string"
+  ) {
+    res.status(404).redirect("/Error");
+  } else {
+    next();
+  }
+};
+
+module.exports = {validateValues}
